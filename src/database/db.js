0370fb081291c212f3f2fe42916324f5c81c76c5
Gabriel Jony <gabjony@gmail.com>
2020-06-06 12:29:37 -0300

    Aula 5 - Concluido Aula 5. A milha extra

---
+++ src/database/db.js
@@ -0,0 +1,84 @@
+// importar a dependência do sqlite 3
+const sqlite3 = require("sqlite3").verbose();
+
+// criar o objeto que irá fazer operações no banco de dados - CRIACAO DO BANCO DE DADOS
+const db = new sqlite3.Database("./src/database/database.db");
+
+module.exports = db;
+
+// CRIANDO AS TABELAS DO BANCO DE DADOS CRIADO ACIMA
+
+// utilizar o objeto de banco de dados para nossas operações
+// db.serialize(() => {
+//com comandos SQL eu vou
+
+// 1 CRIAR uma tabela
+//   db.run(`
+//  CREATE TABLE IF NOT EXISTS places (
+//    id INTEGER PRIMARY KEY AUTOINCREMENT,
+//      image TEXT,
+//      name TEXT,
+//      address TEXT,
+//      address2 TEXT,
+//      state TEXT,
+//      city TEXT,
+//      items TEXT
+//      );
+//  `);
+
+//   // 2 INSERIR dados na tabela
+//   const query = `
+//      INSERT INTO places (
+//      image,
+//      name,
+//      address,
+//      address2,
+//      state,
+//      city,
+//      items
+//      ) VALUES (?,?,?,?,?,?,?);
+//   `;
+
+//   const values = [
+//     "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
+//     "Papersider",
+//     "Guilherme Gemballa, Jardim América",
+//     "Nº 260",
+//     "Santa Catarina",
+//     "Rio do Sul",
+//     "Resíduos Eletrônicos, Lâmpadas",
+//   ];
+
+//   function afterInsertData(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+
+//     console.log("Cadastrado com sucesso!");
+//     console.log(this);
+//   }
+
+//   db.run(query, values, afterInsertData);
+
+//  3 CONSULTANDO os dados da tabela
+
+// db.all(`SELECT name FROM places`, function (err, rows) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log("Aqui estão seus registros: ");
+//   console.log(rows);
+// });
+
+// 4 DELETAR um dado da tabela
+// db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log("Registro deletado com sucesso!");
+// });
+
+// FIM
+// });
--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const server = express();
 
+// pegar o banco de dados
+const db = require("./database/db");
+
 //configurar pasta public
 server.use(express.static("public"));
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }));
 
 //utilizando template engine
 const nunjucks = require("nunjucks");
@@ -27,9 +33,71 @@ server.get("/create-point", (req, res) => {
   return res.render("create-point.html");
 });
 
+// INSERINDO DADOS usando o método post
+server.post("/savepoint", (req, res) => {
+  // req.body: O corpo do nosso formulário
+  // console.log(req.body)
+  // inserir dados no nosso banco de dados
+  const query = `
+    INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+    ) VALUES (?,?,?,?,?,?,?);
+`;
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ];
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no cadastro!");
+    }
+    console.log("Cadastrado com sucesso!");
+    console.log(this);
+
+    return res.render("create-point.html", { saved: true });
+  }
+
+  db.run(query, values, afterInsertData);
+});
+
+// BUSCA OS DADOS DA TABELA
 server.get("/search-results", (req, res) => {
   const search = req.query.search;
-  return res.render("search-results.html", { total: 0 });
+  if (search == "") {
+    // pesquisa vazia
+    return res.render("search-results.html", { total: 0 });
+  }
+
+  // pegar os dados do banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (
+    err,
+    rows
+  ) {
+    if (err) {
+      return console.log(err);
+    }
+
+    //console.log("Aqui estão seus registros: ")
+    //console.log(rows)
+
+    const total = rows.length;
+
+    // mostrar a pãgina html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total: total });
+  });
 });
 
 //ligar o servidor
